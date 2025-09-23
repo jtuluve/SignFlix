c29052652c39261b5b5a95b1c6096d6d39fe1d67
jtuluve <jtuluve@gmail.com>
2025-09-23 11:06:51 +0530

    test

--- conversion/src/functions/processVideo.ts
+++ conversion/src/functions/processVideo.ts
@@ -1,7 +1,7 @@
 import { app, InvocationContext } from "@azure/functions";
 
 export async function processVideo(queueItem: unknown, context: InvocationContext): Promise<void> {
-    context.log('Storage queue function processed work item:', queueItem);
+    context.log('Storage queue function processed work item: ', queueItem);
 }
 
 app.storageQueue('processVideo', {
